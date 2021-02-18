e7c3230fd0cc0b6c0ab0674f6ceaca5f64a77d43
gravitybwlast <ald.dupuis@gmail.com>
2021-02-19 00:11:31 +0100

    Background tests

---
+++ assets/backgrounds/sources/lvl_source1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="lvl_source1" tilewidth="8" tileheight="8" tilecount="400" columns="20">
+ <image source="lvl_source1.png" trans="65ff00" width="160" height="160"/>
+</tileset>
